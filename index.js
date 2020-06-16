97259fa1c814e6375c2c84d0f7f1c29e5eee7599
stackblock <stackchain@gmail.com>
2020-06-16 15:23:44 -0400

    DB Set up

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const server = express();
+const portfolioRoutes = require("./routes/portfolios");
+const mongoose = require("mongoose");
+const config = require("./config/dev");
 
-server.get("/test", (req, res) => {
-  return res.json({ message: "test in working!" });
-});
+mongoose.connect(
+  config.DB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+  (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("Connected to DB");
+    }
+  }
+);
+
+server.use("/api/v1/portfolios", portfolioRoutes);
 
 const PORT = parseInt(process.env.PORT, 10) || 3001;
 server.listen(PORT, (err) => {
